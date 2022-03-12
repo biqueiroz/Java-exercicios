b03bd8c8e70b9421554d8323b3f3a64873158ffc
Bianca Queiroz <biancagqueiroz@gmail.com>
2022-03-12 13:19:07 -0300

    exercicios de java

---
+++ src/Exercicios/Nota.java
@@ -0,0 +1,21 @@
+package Exercicios;
+
+import java.util.Scanner;
+
+public class Nota {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int nota;
+
+        System.out.println("Nota: ");
+        nota = scan.nextInt();
+
+        while(nota < 0 | nota > 10) {
+            System.out.println("Nota inválida! Digite novamente: ");
+            nota = scan.nextInt();
+        }
+
+    }
+
+}
